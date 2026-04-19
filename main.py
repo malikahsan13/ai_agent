b45727b2543989af21cc335e797f0becde802dc3
Malik Ahsan <malikahsan13@gmail.com>
2026-04-19 23:38:11 +0500

    added basic llm starting code

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from ai-agent!")
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from langchain_openai import ChatOpenAI
+from langchain_anthropic import ChatAnthropic
+
+load_dotenv()
+
+# llm = ChatOpenAI(model="gpt-40-mini")
 
 
-if __name__ == "__main__":
-    main()
+llm2 = ChatAnthropic(model="claude-3-5-sonnet-20241022")
+
+response = llm2.invoice("What is the meaning of life?")
+print(response)
