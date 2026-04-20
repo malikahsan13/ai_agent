e332e03ebeb91fc47687ef7129fed99a35700e76
Malik Ahsan <malikahsan13@gmail.com>
2026-04-20 23:43:29 +0500

    added prompt for llm

--- main.py
+++ main.py
@@ -16,4 +16,22 @@ class ResearchResponse(BaseModel):
 llm2 = ChatAnthropic(model="claude-3-5-sonnet-20241022")
 
 response = llm2.invoice("What is the meaning of life?")
+
+parser = PydantincOutputParser(pydantic_object=ResearchResponse)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            You are a research assistant that will help generate a research paper.
+            Answer the user query and use neccessary tolls.
+            Wrap the output in this format and provide no other text\n{format_instructions}
+            """
+        ),
+        ("placeholder","{chat_history}"),
+        ("human","{query}"),
+        ("placeholder","{agent_scratchpad}"),
+    ]
+).partial(format_instructions=parser.get_format_instructions())
 print(response)
