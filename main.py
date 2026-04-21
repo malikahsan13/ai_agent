6d9c58bd8c74f8ed9e136bd17d04b0ddd781fd60
Malik Ahsan <malikahsan13@gmail.com>
2026-04-21 23:54:24 +0500

    added code to structured response

--- main.py
+++ main.py
@@ -47,3 +47,6 @@ agent = create_tool_calling_agent(
 agent_executor = AgentExecutor(agent=agent, tools=[], verbose=True)
 raw_response = agent_executor.invoke({"query":"What is the capital of France?"})
 print(raw_response)
+
+structured_reponse = parser.parse(raw_response.get("output")[0]["text"])
+print(structured_reponse)
