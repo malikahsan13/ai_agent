b72ca1b6bce155c9c344a8d909835374a9888104
Malik Ahsan <malikahsan13@gmail.com>
2026-04-23 00:01:32 +0500

    added save to text method in tools

--- tools.py
+++ tools.py
@@ -4,6 +4,15 @@ from langchain.tools import Tool
 from datetime import datetime
 
 
+def save_to_text(data: str, filename: str= "rearch_output.txt"):
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    formatted_text = f"--- Reasearch Output --- \nTimestamp: {timestamp}\n\n{data}\n\n"
+    
+    with open(filename, "a", encoding="utf-8") as f:
+        f.write(formatted_text)
+        
+    return f"Data successfully saved to {filename}"
+
 search = DuckDuckGoSearchRun()
 search_tool = Tool(
     name="search",
