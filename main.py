c7acce37dd072ceafee9f7fc00844ced0cfa4427
Malik Ahsan <malikahsan13@gmail.com>
2026-04-23 00:03:17 +0500

    created cusom tool save structured format txt file

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
 from langchain.agents import create_tool_calling_agent, AgentExecutor
-from tools import search_tool, wiki_tool
+from tools import search_tool, wiki_tool, save_tool
 
 load_dotenv()
 
@@ -38,7 +38,7 @@ prompt = ChatPromptTemplate.from_messages(
 ).partial(format_instructions=parser.get_format_instructions())
 #print(response)
 
-tools = [search_tool, wiki_tool]
+tools = [search_tool, wiki_tool, save_tool]
 agent = create_tool_calling_agent(
     llm=llm,
     prompt=prompt,
--- tools.py
+++ tools.py
@@ -4,14 +4,20 @@ from langchain.tools import Tool
 from datetime import datetime
 
 
-def save_to_text(data: str, filename: str= "rearch_output.txt"):
+def save_to_text(data: str, filename: str= "research_output.txt"):
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    formatted_text = f"--- Reasearch Output --- \nTimestamp: {timestamp}\n\n{data}\n\n"
+    formatted_text = f"--- Research Output --- \nTimestamp: {timestamp}\n\n{data}\n\n"
     
     with open(filename, "a", encoding="utf-8") as f:
         f.write(formatted_text)
         
     return f"Data successfully saved to {filename}"
+
+save_tool = Tool(
+    name="search",
+    func=save_to_text,
+    description="Saves structured research data to a text file"
+)
 
 search = DuckDuckGoSearchRun()
 search_tool = Tool(
