38fb8ad3176e5078c24325a0182e32fc3d0c8d49
Malik Ahsan <malikahsan13@gmail.com>
2026-04-22 23:53:56 +0500

    added search tool in tools

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
 from langchain.agents import create_tool_calling_agent, AgentExecutor
-
+from tools import search_tool
 
 load_dotenv()
 
@@ -38,13 +38,14 @@ prompt = ChatPromptTemplate.from_messages(
 ).partial(format_instructions=parser.get_format_instructions())
 #print(response)
 
+tools = [search_tool]
 agent = create_tool_calling_agent(
     llm=llm,
     prompt=prompt,
-    tools=[]
+    tools=tools
 )
 
-agent_executor = AgentExecutor(agent=agent, tools=[], verbose=True)
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 raw_response = agent_executor.invoke({"query":"What is the capital of France?"})
 print(raw_response)
 
